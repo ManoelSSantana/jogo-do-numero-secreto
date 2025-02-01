1cb5e72d99c63f42c55f8ff380a4c5e3ee432ac0
ManoelSSantana <manoelsouzasantana704@gmail.com>
2025-02-01 10:56:37 -0300

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ function verificarChute(){
     if (chute == numeroSecreto) {
         exibirTextoNaTela('h1', 'Acertou!');
         let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
-        let mensagemTentativa = `você decubriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+        let mensagemTentativa = `você descubriu o número secreto com ${tentativas} ${palavraTentativa}!`;
         exibirTextoNaTela('p', mensagemTentativa);
         document.getElementById('reiniciar').removeAttribute('disabled'); 
     } else {
